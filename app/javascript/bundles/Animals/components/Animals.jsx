1c5cf6bc5354d6a654624f31e94228e4c9179614
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-11 19:03:06 -0500

    Set images and active state function

--- app/javascript/bundles/Animals/components/Animals.jsx
+++ app/javascript/bundles/Animals/components/Animals.jsx
@@ -21,6 +21,20 @@ export default class Animals extends React.Component {
     };
   }
 
+  loadImages = (animal) => {
+    const client = 'a1aff3b188b6ffd926e3c12232423dcf29f199ea31f5ef7fd561d1f0d998dcb5'
+
+    fetch(`https://api.unsplash.com/search/photos?client_id=${client}&query=${animal.name}`).
+      then(response => response.json()).
+        then(data => {
+          this.setState({
+            active: true,
+            images: data.results
+      });
+    });
+  }
+
+
   render() {
     const {animals} = this.props;
     const {images, active} = this.props;
