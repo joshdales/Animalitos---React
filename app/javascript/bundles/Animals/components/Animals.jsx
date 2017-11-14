7568a886f362db6ba8fee4c85f9e7da43ec194b6
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-14 17:11:19 -0500

    Delete obsolete api key

--- app/javascript/bundles/Animals/components/Animals.jsx
+++ app/javascript/bundles/Animals/components/Animals.jsx
@@ -33,7 +33,7 @@ export default class Animals extends React.Component {
   }
 
   loadImages = (animal) => {
-    const client = 'a1aff3b188b6ffd926e3c12232423dcf29f199ea31f5ef7fd561d1f0d998dcb5'
+    const client = //api key goes here
 
     fetch(`https://api.unsplash.com/search/photos?client_id=${client}&query=${animal.name}`).
       then(response => response.json()).
